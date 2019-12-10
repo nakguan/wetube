2cfd7a505632b007719dda093fefda935d4cd835
nakguan <nakguan@gmail.com>
2019-12-10 11:20:22 +0900

    URL set

--- src/db.js
+++ src/db.js
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 // DB Connetcion
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL_PROD, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
 
 const db = mongoose.connection;
 
